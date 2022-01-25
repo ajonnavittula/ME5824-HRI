ea96c1ca677681cf5cbf55fbbf3b44194999f91d
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-01-25 18:21:52 -0500

    working transitions

--- HW1/transitions.py
+++ HW1/transitions.py
@@ -3,25 +3,26 @@ import numpy as np
 def transitions(s, a, S):
     n_states = len(S)
     P = np.zeros(n_states)
-    s_index = np.where(S==s)
+    s_index = np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]
 
     s1 = s + a
-    if np.where(S==s1):
-        P[np.where(S==s1)] = 0.8
+    # print(np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0])
+    if np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]:
+        P[np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]] = 0.8
     else:
         P[s_index] = 0.8
 
     a1 = np.array([[0, -1], [1, 0]]) * a.T
     s1 = s + a1
-    if np.where(S==s1):
-        P[np.where(S==s1)] = 0.1
+    if np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]:
+        P[np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]] = 0.1
     else:
         P[s_index] += 0.1
 
     a1 = np.array([[0, 1], [-1, 0]]) * a.T
     s1 = s + a1
-    if np.where(S==s1):
-        P[np.where(S==s1)] = 0.1
+    if np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]:
+        P[np.where((S[:,0] == s[0]) & (S[:,1]==s[1]))[0]] = 0.1
     else:
         P[s_index] += 0.1    
 
