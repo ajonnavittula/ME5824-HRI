641390465bfaf334fd37f64149b141f4d31c6c45
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-01-25 17:47:58 -0500

    updated transitions.py

---
+++ HW1/transitions.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def transitions(s, a, S):
+    n_states = len(S)
+    P = np.zeros(n_states)
+    s_index = np.where(S==s)
+
+    s1 = s + a
+    if np.where(S==s1):
+        P(np.where(S==s1)) = 0.8
+    else:
+        P(s_index) = 0.8
+
+    a1 = np.array([[0, -1], [1, 0]]) * a.T
+    s1 = s + a1
+    if np.where(S==s1):
+        P(np.where(S==s1)) = 0.1
+    else:
+        P(s_index) += 0.1
+
+    a1 = np.array([[0, 1], [-1, 0]]) * a.T
+    s1 = s + a1
+    if np.where(S==s1):
+        P(np.where(S==s1)) = 0.1
+    else:
+        P(s_index) += 0.1    
+
+    return P
