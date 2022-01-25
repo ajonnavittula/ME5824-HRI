2f0e9784966bfc42c372d2b283c77e2dc4a12a21
mehtashaunak <mehtashaunak2@gmail.com>
2022-01-25 18:12:02 -0500

    HW1

--- HW1/mdp.py
+++ HW1/mdp.py
@@ -1,7 +1,5 @@
 import numpy as np
 from transitions import transitions
-
-
 S = np.array([[1, 1], [2, 1], [3, 1], [4, 1], [1, 2], [3, 2], [4, 2], [1, 3], [2, 3], [3, 3], [4, 3]])
 
 A = np.array([[1, 0], [-1, 0], [0, 1], [0, -1]])
@@ -12,22 +10,27 @@ R = np.ones(11)*r_empty
 R[10] = 1
 R[6] = -1
 
-V = np.zeros(len(S))
-V1 = np.zeros(len(S))
 
-for interaction in range (10000):
+def main():
+    V = np.zeros(len(S))
+    V1 = np.zeros(len(S))
 
-    for s_idx in range (len(S)):
-        if s_idx == 6 or s_idx == 10:
-            V1[s_idx] = R[s_idx]
-            continue
+    for interaction in range (10000):
 
-        next_state_V = np.zeros(len(A))
-        for a_idx in range (len(A)):
-            next_state_V[a_idx] = np.sum(np.multiply(transitions(S[s_idx,:], A[a_idx,:], S)))
+        for s_idx in range (len(S)):
+            if s_idx == 6 or s_idx == 10:
+                V1[s_idx] = R[s_idx]
+                continue
 
-        V1[s_idx] = R[s_idx] + gamma * max(next_state_V)
+            next_state_V = np.zeros(len(A))
+            for a_idx in range (len(A)):
+                transition = transitions(S[s_idx,:], A[a_idx,:], S)
+                next_state_V[a_idx] = np.sum(np.multiply(transition, V))
 
-    V = V1
+            V1[s_idx] = R[s_idx] + gamma * max(next_state_V)
 
+        V = V1
+    print(V)
 
+if __name__ == "__main__":
+    main()
